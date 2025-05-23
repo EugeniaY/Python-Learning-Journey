c32d40925dd08d61512a07eeecf8b2f963beb245
EugeniaY <165252404+EugeniaY@users.noreply.github.com>
2025-05-23 15:06:36 +0800

    Add files via upload

---
+++ Python/RecievingInput.py
@@ -0,0 +1,5 @@
+#Ask two questions: person's name and favorite color.
+#Then, print a message like "Eugenia likes purple"
+name = input("Please enter your name: ")
+color = input("Please enter your favorite color: ")
+print(f"{name} likes {color}")
